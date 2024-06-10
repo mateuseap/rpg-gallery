0ffae7c70c180522b06ef18f964d2fe24e4a67b0
Mateus Elias <meap@cin.ufpe.br>
2024-06-10 19:36:09 -0300

    feat(frontend/src/pages/Home): use Tabs component

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import DefaultPage from "../../components/DefaultPage";
 import World from "../../components/World";
 import { IWorld } from "../../types";
 import Spinner from "../../components/Spinner";
+import Tabs from "../../components/Tabs";
 
 export default function Home() {
-  const { data, isLoading } = useQuery<{ worlds: Array<IWorld> }>({
-    queryKey: [`worlds`, "GET"],
+  const [selectedLanguage, setSelectedLanguage] = useState<string>("en");
+
+  const { data, isLoading } = useQuery<{ worlds: IWorld[] }>({
+    queryKey: [`worlds?language=${selectedLanguage}`, "GET"],
     retry: false,
   });
 
@@ -25,9 +29,17 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <Tabs
+        tabs={[
+          { label: "English", value: "en" },
+          { label: "Portuguese", value: "pt-br" },
+        ]}
+        selectedTab={selectedLanguage}
+        onChange={(language) => setSelectedLanguage(language)}
+      />
       {isLoading ? (
-        <div className="py-4 sm:py-8">
-          <Spinner size="xl" />
+        <div className="py-8">
+          <Spinner />
         </div>
       ) : (
         data?.worlds.map((world) => <World key={world.title} {...world} />)
